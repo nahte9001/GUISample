12de02f39a381fa36592b5e47c4f89174b3d98e8
Ethan Torres <nahte9001@gmail.com>
2018-11-27 08:27:56 -0700

    imported sample.view.Sampleframe in GUIController

--- src/sample/controller/GUIController.java
+++ src/sample/controller/GUIController.java
@@ -1,7 +1,15 @@
 package sample.controller;
 
+import sample.view.SampleFrame;
+
 public class GUIController
 {
+	private SampleFrame appFrame;
+	
+	public GUIController()
+	{
+		this.appFrame = new SampleFrame(this);
+	}
 	
 	public void start()
 	{
